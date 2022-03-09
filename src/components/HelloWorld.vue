08d260f1f5a80b9ffbe94dd02bc834f716783540
thiagof-dev <thiago.customtrade@gmail.com>
2022-03-09 16:13:02 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore() 
+
+const {counter, doubleCount} = storeToRefs(store)
+
+const {reset} = store
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ counter }}</h1>
+    <h3>
+      This number above, comes from a pinia store
+    </h3>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore() 
+
+const {counter, doubleCount} = storeToRefs(store)
+
+const {increment} = store
+
+</script>
+
+<template>
+<h1>Count: {{counter}}</h1>
+<h2>Double count: {{doubleCount}}</h2>
+<button @click="increment"> Add + 1</button>
+<br>
+<button>Resset</button>
+</template>
